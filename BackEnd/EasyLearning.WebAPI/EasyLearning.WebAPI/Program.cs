64767d8d0e4ce622aaaa74ce753f642c22d17323
TRONG VUONG <tronghandsome1111@gmail.com>
2024-03-31 15:40:32 +0700

    update sample with React

--- BackEnd/EasyLearning.WebAPI/EasyLearning.WebAPI/Program.cs
+++ BackEnd/EasyLearning.WebAPI/EasyLearning.WebAPI/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000/");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,4 +30,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(origin => true)
+                    .AllowCredentials());
 app.Run();
