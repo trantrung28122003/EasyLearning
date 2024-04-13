42f1d35eba0b5fed11a3fd026bf01896d5615e3a
TRONG VUONG <tronghandsome1111@gmail.com>
2024-04-13 15:59:16 +0700

    update area

---
+++ BackEnd/EasyLearning/EasyLearning.Infrastructure/Data/Entities/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyLearning.Infrastructure.Data.Entities
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+    }
+}
---
+++ BackEnd/EasyLearning/EasyLearning.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,126 @@
+﻿using EasyLearning.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EasyLearning.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet, AllowAnonymous]
+        public IActionResult Register()
+        {
+            UserRegistrationViewModel model = new UserRegistrationViewModel();
+            return View(model);
+        }
+
+        [HttpPost, AllowAnonymous]
+        public async Task<IActionResult> Register(UserRegistrationViewModel request)
+        {
+            if (ModelState.IsValid)
+            {
+                var userCheck = await userManager.FindByEmailAsync(request.Email);
+                if (userCheck == null)
+                {
+                    var user = new IdentityUser
+                    {
+                        UserName = request.Email,
+                        NormalizedUserName = request.Email,
+                        Email = request.Email,
+                        PhoneNumber = request.PhoneNumber,
+                        EmailConfirmed = true,
+                        PhoneNumberConfirmed = true,
+                    };
+                    var result = await userManager.CreateAsync(user, request.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Login");
+                    }
+                    else
+                    {
+                        if (result.Errors.Count() > 0)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("message", error.Description);
+                            }
+                        }
+                        return View(request);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("message", "Email already exists.");
+                    return View(request);
+                }
+            }
+            return View(request);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            UserLoginViewModel model = new UserLoginViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if (user != null && !user.EmailConfirmed)
+                {
+                    ModelState.AddModelError("message", "Email not confirmed yet");
+                    return View(model);
+
+                }
+                if (await userManager.CheckPasswordAsync(user, model.Password) == false)
+                {
+                    ModelState.AddModelError("message", "Invalid credentials");
+                    return View(model);
+
+                }
+
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, true);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddClaimAsync(user, new Claim("UserRole", "Admin"));
+                    return RedirectToAction("Dashboard");
+                }
+                else if (result.IsLockedOut)
+                {
+                    return View("AccountLocked");
+                }
+                else
+                {
+                    ModelState.AddModelError("message", "Invalid login attempt");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("login", "account");
+        }
+
+    }
+}
