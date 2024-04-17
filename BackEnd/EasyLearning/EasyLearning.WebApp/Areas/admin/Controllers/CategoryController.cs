28dba6f5585316777718a13a03b814e7a0cb23f7
DuongMinhThanh <duongminhthanh28@gmail.com>
2024-04-17 18:08:16 +0700

    update file

--- BackEnd/EasyLearning/EasyLearning.WebApp/Areas/admin/Controllers/CategoryController.cs
+++ BackEnd/EasyLearning/EasyLearning.WebApp/Areas/admin/Controllers/CategoryController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EasyLearning.WebApp.Areas.admin.Controllers
 {
     [Area("admin")]
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "User")]
     public class CategoryController : Controller
     {
         private readonly ICourseService _courseService;
